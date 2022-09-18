d265c5afac190b57cf18e30c887351a5f8231eb7
kalinin <kalinin.nikita344@yandex.ru>
2022-09-18 22:32:40 +0300

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ViewModeComponent } from './view-mode/view-mode.component';
-import { SettingsComponent } from './settings/settings.component';
+import { ViewModeComponent } from 'src/app/view-mode/view-mode.component';
+import { SettingsComponent } from 'src/app/settings/settings.component';
 
 const routes: Routes = [{ path: 'view-mode', component: ViewModeComponent },
 { path: 'settings', component: SettingsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,18 @@ import {StoreRouterConnectingModule} from '@ngrx/router-store'
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { ViewModeComponent } from './view-mode/view-mode.component';
-import { SettingsComponent } from './settings/settings.component';
+import { ViewModeComponent } from 'src/app/view-mode/view-mode.component';
+import { SettingsComponent } from 'src/app/settings/settings.component';
 import { appReducers } from 'src/store/reducers/app.reducer';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { HighchartComponent } from './highchart/highchart.component';
+import { HighchartComponent } from 'src/app/highchart/highchart.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ModalComponent } from './modal/modal.component';
+import { ModalComponent } from 'src/app/modal/modal.component';
 import { InputMaskModule } from '@ngneat/input-mask';
-import { ChartDataService } from './services/chartdata.service';
+import { ChartDataService } from 'src/app/services/chartdata.service';
 
 @NgModule({
   declarations: [
